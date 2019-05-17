773bf9b5e90e3d79b8ba50c1a3fba1adb8992320
Gustaf Buchanan <gnhbuchanan@gmail.com>
2019-05-17 20:39:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,33 @@ function shout(word){
   return word.toUpperCase();
   
 }
+
+function whisper(word){
+  
+  return word.toLowerCase();
+  
+}
+
+function logShout(word){
+  
+  console.log(word.toUpperCase());
+  
+}
+
+function logWhisper(word){
+  
+  console.log(word.toLowerCase());
+  
+}
+
+const sayHiToGrandma = words =>{
+  
+  
+  
+}
+
+
+
+
+
+
